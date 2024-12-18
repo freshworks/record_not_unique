fd786d45a30df8e06daec01685d1b6172340dec9
mohammed-ridwan <mohammed.ridwan@freshworks.com>
2024-12-18 14:30:25 +0530

    rails version check added for errors.add

--- lib/record_not_unique.rb
+++ lib/record_not_unique.rb
@@ -58,8 +58,11 @@ module RecordNotUnique
 						custom_error = self.class._rnu_error_messages[i]
 						object = custom_error.last
 						custom_error_msg = object.is_a?(Proc) ? object.call : object
-						
-						self.errors.add(custom_error.first, message: custom_error_msg)
+						if Rails::VERSION::MAJOR >= 6
+							self.errors.add(custom_error.first, message: custom_error_msg)
+						else
+							self.errors.add(custom_error.first, custom_error_msg)
+						end
 					end
 				}
 				false
